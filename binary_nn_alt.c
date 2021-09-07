e295ed4720b04a173df244f268ff46756dac0b78
noeblassel <62473139+noeblassel@users.noreply.github.com>
2021-09-07 12:35:51 +0200

    Add files via upload

--- binary_nn_alt.c
+++ binary_nn_alt.c
@@ -22,15 +22,16 @@ NETWORK PARAMETERS ARE UPDATED USING SIMPLE ONLINE GRADIENT DESCENT.*/
             Layer* next;//POINTER TO NEXT LAYER (NULL if output layer)
             Neuron* units;//POINTER TO FIRST NEURON IN LAYER
 
-            real** incoming_weights;//MATRIX OF CONNECTION WEIGHTS BETWEEN PREVIOUS AND CURRENT LAYER
+            real** weights;//MATRIX OF CONNECTION WEIGHTS BETWEEN PREVIOUS AND CURRENT LAYER
             real* bias;//VECTOR OF BIASES (CONNECTIONS WEIGHTS BETWEEN BIAS NEURON AND CURRENT LAYER)
 
-            real** weights_grad;//GRADIENT OF LOSS W.R.T. CONNECTION WEIGHTS (same shape as incoming_weights)
+            real** weights_grad;//GRADIENT OF LOSS W.R.T. CONNECTION WEIGHTS (same shape as weights)
             real* bias_grad;//GRADIENT OF LOSS W.R.T. BIASES
         }Layer;
 
         typedef struct{
             int n_layers;//NUMBER OF LAYERS
+            int* layer_dims;
             Layer* layers;//POINTER TO THE FIRST LAYER
         }MultiLayerPerceptron;
 
@@ -41,13 +42,13 @@ NETWORK PARAMETERS ARE UPDATED USING SIMPLE ONLINE GRADIENT DESCENT.*/
 real eta=.5;//LEARNING RATE
 long r_state=1103527590;//INITIAL RANDOM SEED
 
-int dim_n,dim_out,n_hidden_layers;
+int dim_in,dim_out,n_hidden_layers;
 int*dims;
 
 int n_epochs;
-int n_training_examples,n_testing_inputs;
+int n_training_examples,n_test_inputs;
 char** testing_inputs;
-char** training_examples[2];//TRAINING (X,Y) PAIRS
+char*** training_examples;//TRAINING (X,Y) PAIRS
 
 MultiLayerPerceptron net;
 
@@ -106,5 +107,51 @@ real sigmoid(real x){//only deal with [0,1] floating points to avoid overflow er
 
 /*=================MEMORY HANDLING AND INPUT PARSING========================*/
     void setup(){
-        
+        scanf("%d%d%d%d%d%d",&dim_in,&dim_out,&n_hidden_layers,&n_test_inputs,&n_training_examples,&n_epochs);
+
+        net.n_layers=2+n_hidden_layers;
+        net.layers=(Layer*)malloc(net.n_layers*sizeof(Layer));
+        net.layer_dims=(int*)malloc((2+n_hidden_layers)*sizeof(int));
+
+        net.layer_dims[0]=dim_in;
+        net.layer_dims[1+n_hidden_layers]=dim_out;
+
+        for(int i=1;i<=n_hidden_layers;++i){
+            scanf("%d",net.layer_dims+i);
+        }
+
+        testing_inputs=(char**)malloc(n_test_inputs*sizeof(char*));
+        training_examples=(char***)malloc(n_training_examples*sizeof(char**));
+
+        for(int i=0;i<n_test_inputs;i++){
+            testing_inputs[i]=(char*)malloc(dim_in*sizeof(char));
+            scanf("%s",testing_inputs[i]);
+        }
+
+        for(int i=0;i<n_training_examples;++i){
+            training_examples[i]=(char**)malloc(2*sizeof(char*));
+            training_examples[i][0]=(char*)malloc(dim_in*sizeof(char));
+            training_examples[i][1]=(char*)malloc(dim_out*sizeof(char));
+            scanf("%s%s",training_examples[i][0],training_examples[i][1]);
+        }
+
+        for(int i=0;i<net.n_layers;++i){
+            net.layers[i].units=(Neuron*)malloc(net.layer_dims[i]*sizeof(Neuron));
+            if(i>0){
+                net.layers[i].prev=net.layers+i-1;
+                net.layers[i].weights=(real**)malloc(net.layer_dims[i-1]*sizeof(real*));
+                net.layers[i].weights_grad=(real**)malloc(net.layer_dims[i-1]*sizeof(real*));
+
+                for(int j=0;j<net.layer_dims[i-1];j++){
+                    net.layers[i].weights[j]=(real*)malloc(net.layer_dims[i]*sizeof(real));
+                    net.layers[i].weights_grad[j]=(real*)malloc(net.layer_dims[i]*sizeof(real));
+
+                }
+
+                net.layers[i].bias=(real*)malloc(net.layer_dims[i]*sizeof(real));
+                net.layers[i].bias_grad=(real*)malloc(net.layer_dims[i]*sizeof(real));
+
+            }
+            if(i<net.n_layers-1)net.layers[i].next=net.layers+i+1;
+        } 
     }
